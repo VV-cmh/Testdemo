ca7a461e8f86a058a90939a04367c6f5d3e5e09c
Mikey <mikey.chen@ourleadchina.com>
2022-09-22 09:03:49 +0800

    Initial commit

---
+++ src/main/java/com/example/config/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.formLogin()
+                .loginPage("/loginPage.html")
+                .loginProcessingUrl("/form/login");
+                //.passwordParameter("password")
+                //.usernameParameter("username");
+        http.authorizeRequests().antMatchers("/form/login","/loginPage.html").permitAll()
+                .anyRequest().authenticated();
+    }
+}
